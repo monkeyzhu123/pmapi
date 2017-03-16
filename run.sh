b0e516fb763c938fd97f3f54c14d3f2700bb96df
monkeyzhu <monkeyzhu333@gmail.com>
2017-03-16 15:17:21 +0800

    change script mode

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 pip install -r requirements.txt
 cp logrotate_gunicorn /etc/logrotate.d/
-cp gunicorn /etc/init.d/gunicorn
+cp gunicorn /etc/init.d/gunicorn;chmod +x /etc/init.d/gunicorn
 /etc/init.d/gunicorn start
